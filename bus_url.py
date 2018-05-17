dbdb834055dc6d23a015d8188d118a7da7a9a6e6
aregkarapetyan <38075172+aregkarapetyan@users.noreply.github.com>
2018-05-17 10:07:32 +0400

    Add files via upload

---
+++ bus_url.py
@@ -0,0 +1,24 @@
+# Include bus data needed for the url
+
+bus_url_0 = '0'
+bus_url_1 = 'https://maps.googleapis.com/maps/api/directions/json?origin=Khaghagh+Don+St+Yerevan&destination=Dro+St+Yerevan&waypoints=Titogradian+St|Ayvazovskiy+St|11+Erebuni+St|2+Artsakh+Ave|8+Artsakh+Ave|65+Tigran+Mets+Ave|40.163964,44.512981|40.171391,44.513615|17+Tigran+Mets+Ave|Republic+Square+Yerevan|Amiryan+St|Mesrop+Mashtots+Ave|2+Marshal+Baghramyan+Ave|40.190118,44.512753|40.192171,44.504436|Barekamutyan+Overpass|Kasyan+St|29+Komitas+Ave|52+Komitas+Ave|40.205730,44.525919|40.205814,44.526322|40.210834,44.533352|Paruyr+Sevak+St&key={}'
+bus_url_2 = '0'
+bus_url_3 = 'https://maps.googleapis.com/maps/api/directions/json?origin=Arno+Babajanyan+St+Yerevan&destination=Vilnius+St+Yerevan&waypoints=84+Raffi+St+Yerevan|40.174414,44.449444|40.174560,44.453409|23+Sebastia+St+Yerevan|40.186154,44.461680|40.190225,44.462077|Leningradyan+St|40.1875928,44.4769873|14+Kievyan+St|Kasyan+St|29+Komitas+Ave|52+Komitas+Ave|Davit+Anhaght+St|6+Rubinyants+St|40.201805,44.544912|40.203536,44.552776|Gai+Ave+Yerevan|40.196279,44.568473|40.193908,44.569095|40.192138,44.568271|Badal+Muradian+St|Davit+Bek+St|Hunan+Avetisyan+St&key={}'
+bus_url_4 = '0'
+bus_url_5 = 'https://maps.googleapis.com/maps/api/directions/json?origin=Kochinyan+St+Yerevan+Armenia&destination=Kilikia+Bus+Stop+Admiral+Isakov+Avenue+Yerevan&waypoints=Kochinyan+St+Yerevan+Armenia|Hovhannisian+St+Yerevan+Armenia|Bakunts+St+Yerevan+Armenia|40.1891675,44.5689981|Gai+Ave+Yerevan+Armenia|Myasnikyan+Ave+Yerevan+Armenia|40.1861465,44.5217571|Moskovyan+Street+Yerevan+Armenia|Mesrop+Mashtots+Ave+Yerevan+Armenia|Victory+Bridge+Yerevan+Armenia|40.175728,44.4982817|Kilikia+Bus+Stop+Admiral+Isakov+Avenue+Yerevan&key={}'
+bus_url_6 = '0'
+bus_url_7 = 'https://maps.googleapis.com/maps/api/directions/json?origin=Khaghagh+Don+St+Yerevan+Armenia&destination=26+Yeghvard+Hwy+Yerevan+Armenia&waypoints=Titogradian+St+Yerevan+Armenia|Ayvazovskiy+St+Yerevan+Armenia|40.142184,44.5177589|Atoyan+St+Yerevan+Armenia|Belinskiy+St+Yerevan+Armenia|Sasuntsi+Davit+St+Yerevan+Armenia|Tigran+Mets+Ave+Yerevan+Armenia|40.1780361,44.5228827|Tumanyan+St+Yerevan+Armenia|Koryun+St+Yerevan+Armenia|40.1900263,44.5264234|40.1926386,44.5330408|Saralanj+Ave+Yerevan+Armenia|40.198793,44.5018501|Vagharsh+Vagharshyan+St+Yerevan+Armenia|40.2124785,44.4909302|40.216866,44.487015|Tigran+Petrosyan+St+Yerevan+0054+Armenia&key={}'
+bus_url_8 = 'https://maps.googleapis.com/maps/api/directions/json?origin=Tigran+Petrosyan+St+Yerevan+0054+Armenia&destination=40.1726044,44.5608591&waypoints=Iosifian+St+Yerevan+Armenia|19+Halabyan+Street+Yerevan|24+Kievyan+St+Yerevan+Armenia|Marshal+Baghramyan+Ave+Yerevan+Armenia|Moskovyan+St+Yerevan+Armenia|40.187769,44.515961|40.1870065,44.5227706|27+Abovyan+St+Yerevan+Armenia|40.1911598,44.5277547|62+Sharimanyan+St+Yerevan+Armenia|40.1932351,44.5341107|40.2057359,44.5584736+Myasnikyan+Ave+Yerevan+Armenia|Gai+Ave+Yerevan+Armenia|40.189267,44.567677|40.1877227,44.5647146|40.1803136,44.5612896|Hunan+Avetisyan+St+Yerevan+Armenia|40.1726044,44.5608591&key={}'
+bus_url_9 = '0'
+bus_url_10 = '0'
+bus_url_11 = ''
+bus_url_12 = ''
+bus_url_13 = ''
+bus_url_14 = ''
+bus_url_15 = ''
+bus_url_16 = ''
+bus_url_17 = ''
+bus_url_18 = 'https://maps.googleapis.com/maps/api/directions/json?origin=&destination=&waypoints=&key={}'
+bus_url_19 = ''
+
+bus_url = [bus_url_0,bus_url_1,bus_url_2,bus_url_3,bus_url_4,bus_url_5,bus_url_6,bus_url_7,bus_url_8,bus_url_9,bus_url_10,bus_url_11,bus_url_12,bus_url_13,bus_url_14,bus_url_15,bus_url_16,bus_url_17,bus_url_18,bus_url_19]
---
+++ map_draw.py
@@ -0,0 +1,119 @@
+import json, urllib2, webbrowser
+from gmplot import gmplot
+from bus_url import *
+from bus_streets import *
+
+# List of variables needed for the execution of the code
+API_KEY = 'AIzaSyBAG6CASjfND1ApXO65d7mtpuP8Cty4fdc'
+color = ['red','blue','green','yellow','black']
+
+# Search for the origin and destination
+# Get valid transport which includes both origin and destination
+# Get index of each street in the list
+def search_street(origin, destination):
+    valid_transport = []
+    valid_indexs = []
+    for i in range(len(bus_str)):
+        found_locations = []
+        valid = 0
+        for j in range (len(bus_str[i])):
+            if str(bus_str[i][j]) == str(origin) or str(bus_str[i][j]) == str(destination):
+                valid = valid + 1
+                found_locations.append(j)
+        if valid == 2:
+            valid_transport.append(i)
+            valid_indexs.append(found_locations)
+    return valid_transport, valid_indexs
+
+# Finds shoretes route out of all routes
+def find_shortest(valid_transport, valid_index):
+    difference = []
+    for i in range (len(valid_index)):
+        diff = valid_index[i][1]-valid_index[i][0]
+        difference.append(diff)
+    try:
+        min_index = difference.index(min(difference))
+        fastest_transport = valid_transport[min_index]
+    except Exception as e:
+        print "No valid transport for your locations"
+        fastest_transport = "NONE"
+    #fastest_transport = valid_transport[min_index]
+    return fastest_transport
+
+# Use Google API and get url for further use
+def route_url(API_KEY, bus_num):
+    url = urllib2.urlopen(bus_url[bus_num].format(API_KEY))
+    route_url_data = json.load(url)
+    return route_url_data
+
+# Gets list of coordinates from the given url
+def get_locations(route_url_data,API_KEY):
+    start_location = []
+    #print route_url_data
+    for i in range(len(route_url_data['routes'][0]['waypoint_order'])+1):
+        for element in route_url_data['routes'][0]['legs'][i]['steps']:
+            start_location.append([element['start_location']['lat'], element['start_location']['lng'],'|'])
+            start_location.append([element['end_location']['lat'], element['end_location']['lng'],'|'])
+    #print start_location
+    locations = []
+    for i in start_location[1::2]:
+        locations.append(str(i[0]) + ',' + str(i[1]) + str(i[2]))
+    return locations
+
+# Gets valid link for the plotting
+def get_road_data(locations,API_KEY):
+    url = ('https://roads.googleapis.com/v1/snapToRoads?path={}&interpolate=true&key={}'.format(''.join(locations), API_KEY))
+    url = url.rsplit('|',1)
+    url = ''.join(url)
+    url = urllib2.urlopen(url)
+
+    url_data = json.load(url)
+
+    roads = []
+    for elem in url_data['snappedPoints']:
+        roads.append((elem['location']['latitude'], elem['location']['longitude']))
+    return roads
+
+# Plots the routes of the valid transport
+def plot(road_draw, valid_transport, color):
+    gmap = gmplot.GoogleMapPlotter.from_geocode("Yerevan")
+    for i in range (len(valid_transport)):
+        place_lats, place_lons = zip(*road_draw[i])
+        gmap.plot(place_lats, place_lons, color[i], edge_width=10)
+        gmap.draw("my_map.html")
+
+def main():
+    print "<-<-<-<-< Welcome to TranspoRoute >->->->->"
+    origin = raw_input("Input your origin point: ")
+    destination = raw_input("Input your destination point: ")
+
+    valid_transport, valid_index = search_street(origin, destination)
+    valid_transport_str = ''
+    for i in range (len(valid_transport)):
+        valid_transport_str = valid_transport_str + str(valid_transport[i]) + ', '
+    valid_transport_str = valid_transport_str[:len(valid_transport_str)-2]
+    print "You can use bus/buses " + valid_transport_str + " to get from " + origin + " to " + destination
+    fastest_transport = find_shortest(valid_transport, valid_index)
+    print "Bus which will help you get from " + origin + " to " + destination + " in the shortest time is #" + str(fastest_transport)
+    #print "Fastest bus route is marked on the map in <<" + color[valid_transport.index(fastest_transport)] + ">>"
+    road_draw = []
+    for i in range (len(valid_transport)):
+        bus_num = valid_transport[i]
+        route_url_data = route_url(API_KEY, bus_num)
+        locations = get_locations(route_url_data, API_KEY)
+        roads = get_road_data(locations, API_KEY)
+        road_draw.append(roads)
+    plot_check = 0
+    e = ""
+    while plot_check != 1:
+        try:
+            plot(road_draw, valid_transport, color)
+        except Exception as e:
+            continue
+            #print "Wait a little bit"
+        if e != "list index out of range":
+            plot_check = 1
+    webbrowser.open_new_tab("my_map.html")
+    print "Your default browser should open the map with the routes, if not, please check browser settings or open file 'my_map.html', which is in the same location as the python file"
+
+main()
