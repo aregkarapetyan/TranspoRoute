12e7ef06795db6083c86ce3d98d672d9e1d7f58f
aregkarapetyan <38075172+aregkarapetyan@users.noreply.github.com>
2018-05-17 23:26:31 +0400

    Add files via upload
    Plots only if there is valid transport
    Code optimization

--- map_draw.py
+++ map_draw.py
@@ -35,9 +35,8 @@ def find_shortest(valid_transport, valid_index):
         min_index = difference.index(min(difference))
         fastest_transport = valid_transport[min_index]
     except Exception as e:
-        print "No valid transport for your locations"
+        print "Sorry, no valid transport for your locations"
         fastest_transport = "NONE"
-    #fastest_transport = valid_transport[min_index]
     return fastest_transport
 
 # Use Google API and get url for further use
@@ -49,12 +48,10 @@ def route_url(API_KEY, bus_num):
 # Gets list of coordinates from the given url
 def get_locations(route_url_data,API_KEY):
     start_location = []
-    #print route_url_data
     for i in range(len(route_url_data['routes'][0]['waypoint_order'])+1):
         for element in route_url_data['routes'][0]['legs'][i]['steps']:
             start_location.append([element['start_location']['lat'], element['start_location']['lng'],'|'])
             start_location.append([element['end_location']['lat'], element['end_location']['lng'],'|'])
-    #print start_location
     locations = []
     for i in start_location[1::2]:
         locations.append(str(i[0]) + ',' + str(i[1]) + str(i[2]))
@@ -91,10 +88,7 @@ def main():
     for i in range (len(valid_transport)):
         valid_transport_str = valid_transport_str + str(valid_transport[i]) + ', '
     valid_transport_str = valid_transport_str[:len(valid_transport_str)-2]
-    print "You can use bus/buses " + valid_transport_str + " to get from " + origin + " to " + destination
     fastest_transport = find_shortest(valid_transport, valid_index)
-    print "Bus which will help you get from " + origin + " to " + destination + " in the shortest time is #" + str(fastest_transport)
-    #print "Fastest bus route is marked on the map in <<" + color[valid_transport.index(fastest_transport)] + ">>"
     road_draw = []
     for i in range (len(valid_transport)):
         bus_num = valid_transport[i]
@@ -109,10 +103,13 @@ def main():
             plot(road_draw, valid_transport, color)
         except Exception as e:
             continue
-            #print "Wait a little bit"
         if e != "list index out of range":
             plot_check = 1
-    webbrowser.open_new_tab("my_map.html")
-    print "Your default browser should open the map with the routes, if not, please check browser settings or open file 'my_map.html', which is in the same location as the python file"
+    if fastest_transport != "NONE":
+        print "Bus which will help you get from " + origin + " to " + destination + " in the shortest time is #" + str(fastest_transport)
+        print "Fastest bus route is marked on the map in <<" + color[valid_transport.index(fastest_transport)] + ">>"
+        print "You can use bus/buses " + valid_transport_str + " to get from " + origin + " to " + destination
+        webbrowser.open_new_tab("my_map.html")
+        print "Your default browser should open the map with the routes, if not, please check browser settings or open file 'my_map.html', which is in the same location as the python file"
 
 main()
